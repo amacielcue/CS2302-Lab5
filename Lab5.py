b1940526450862f1c21ffac1266a141604b59578
amacielcue <43260850+amacielcue@users.noreply.github.com>
2018-11-27 23:35:34 -0700

    Add files via upload

---
+++ Lab5.py
@@ -0,0 +1,22 @@
+class Heap:
+    def _init_ (self):
+        self.heap_array = []
+
+    def insert (self, k):
+        self.heap_array.append(k)
+
+
+
+    def extract_min(selfself):
+        if self.is_empty():
+            return None
+
+        min_elem = self.heap_array.append[0]
+
+
+
+
+        return min_elem
+
+    def is_empty(self):
+        return len(self.heap_array) == 0
